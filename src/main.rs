f6f71955f66f9e07fe62736177270056c93d95e3
potatoboiler <1nova2hunter3@gmail.com>
2022-04-07 17:24:33 -0700

    change to f64

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,22 @@ impl Student {
     pub fn new(iq: f64) -> Self {
         Self { iq: iq }
     }
-    pub fn take_test(test: Test<impl Float, impl WeightType>) -> f64 {
+    pub fn take_test(test: Test) -> f64 {
         1.0
     }
 }
 
-struct Test<D, W>
-where
-    D: Float,
-    W: WeightType,
+struct Test
 {
-    difficulties: Vec<D>, // Bernoulli probabilities of passing the question
-    weights: Vec<W>, // point weights on the test, by default all questions are uniformly weighted
-    total_weight: W,
+    difficulties: Vec<f64>, // Bernoulli probabilities of passing the question
+    weights: Vec<f64>, // point weights on the test, by default all questions are uniformly weighted
+    total_weight: f64,
 }
-impl<D, W> Test<D, W>
-where
-    D: Float,
-    W: WeightType,
+impl Test
 {
-    pub fn new(difficulties: Vec<D>, weights: Vec<W>) -> Self {
+    pub fn new(difficulties: Vec<f64>, weights: Vec<f64>) -> Self {
         assert_eq!(difficulties.len(), weights.len());
-        // assert!(weights.iter().all(|x| *x > 0));
+        assert!(weights.iter().all(|x| *x > 0.0));
 
         let total = weights.iter().copied().sum();
 
